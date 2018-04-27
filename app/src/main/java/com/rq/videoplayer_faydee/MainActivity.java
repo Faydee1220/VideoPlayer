9201907e8d4b3fbe7ea7eabd344f7e3ff9b94e2c
Faydee <faydee1220@gmail.com>
2018-04-27 09:27:45 +0800

    add video url

--- app/src/main/java/com/rq/videoplayer_faydee/MainActivity.java
+++ app/src/main/java/com/rq/videoplayer_faydee/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String mVideoUrl = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
+//    private String mVideoUrl = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/protraitVideo.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
